1db9bd24d4a9250d2fa7b437ff0916e45fbf9e07
hidaken991018 <ed.hidaken@gmail.com>
2023-06-04 13:56:33 +0900

    storybook修正

--- src/components/atoms/Button.stories.ts
+++ src/components/atoms/Button.stories.ts
@@ -1,46 +1,38 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
 import { Button } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Button> = {
-  title: 'Example/Button',
+  title: 'Atoms/Button',
   component: Button,
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: {
-      control: 'color',
-    },
-  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button',
+    innerText: 'Button',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    innerText: 'Button',
   },
 };
 
 export const Large: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    fontSize: 'lg',
+    innerText: 'Button',
   },
 };
 
 export const Small: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    fontSize: 'sm',
+    innerText: 'Button',
   },
 };
